ca201ed0bf02044e5d40ba6c37b7ca595d4fe121
The Retro Bristolian <106443135+theretrobristolian@users.noreply.github.com>
2026-08-29 15:34:39 +0100

    Enable BIOS framebuffer console

---
+++ config/local/console.h
@@ -0,0 +1,10 @@
+/*
+ * Local console configuration for the legacy BIOS build.
+ */
+
+#ifndef CONFIG_LOCAL_CONSOLE_H
+#define CONFIG_LOCAL_CONSOLE_H
+
+#define CONSOLE_FRAMEBUFFER
+
+#endif /* CONFIG_LOCAL_CONSOLE_H */
